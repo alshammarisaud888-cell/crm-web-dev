58dd869d74f5b17651a385968fd555d0cb00a301
alshammarisaud888-cell <alshammarisaud888@gmail.com>
2026-08-24 00:40:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>CRM Web</h1>
-    <h2>Application is running successfully!</h2>
-    <p>Welcome to our CRM system.</p>
-    """
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
